489784622efe4e3c73fe8d553987ef5d7eab79e8
ronalgope <ronal@icube.us>
2017-04-24 01:23:02 +0700

    reupload all

--- app/code/core/Mage/Directory/Model/Currency.php
+++ app/code/core/Mage/Directory/Model/Currency.php
@@ -20,7 +20,7 @@
  *
  * @category    Mage
  * @package     Mage_Directory
- * @copyright  Copyright (c) 2006-2017 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -219,7 +219,7 @@ class Mage_Directory_Model_Currency extends Mage_Core_Model_Abstract
      */
     public function format($price, $options = array(), $includeContainer = true, $addBrackets = false)
     {
-        return $this->formatPrecision($price, 2, $options, $includeContainer, $addBrackets);
+        return $this->formatPrecision($price, 0, $options, $includeContainer, $addBrackets);
     }
 
     /**
